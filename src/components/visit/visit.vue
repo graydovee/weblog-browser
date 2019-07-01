6b5eb422497d1e8b681888c07b23e7409fd64201
graydovee <1036308853@qq.com>
2019-07-01 15:26:32 +0800

    create router

---
+++ src/components/visit/visit.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'visit'
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,8 @@ import Router from 'vue-router'
 import admin from '@/components/admin/admin'
 import login from '@/components/admin/login'
 import register from '@/components/admin/register'
+import visit from '@/components/visit/visit'
+import home from '@/components/home/home'
 
 Vue.use(Router)
 
@@ -29,6 +31,16 @@ export default new Router({
           component: register
         }
       ]
+    },
+    {
+      path: '/home',
+      name: 'home',
+      component: home
+    },
+    {
+      path: '/visit',
+      name: 'visit',
+      component: visit
     }
   ]
 })
